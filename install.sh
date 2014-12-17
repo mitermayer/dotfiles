90b4b172eefe1212e4c847b1d5258f3eb9b8b140
Miter Reis <mreis@atlassian.com>
2014-12-17 11:07:00 +1100

    Adding vundle steps for auto installing vim plugins

--- install.sh
+++ install.sh
@@ -6,7 +6,13 @@
 
 ########## Variables
 
-dir=~/repo/mitermayer/dotfiles                    # dotfiles directory
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 olddir=~/dotfiles_old             # old dotfiles backup directory
 files="vimrc bash_aliases aliases functions"    # list of files/folders to symlink in homedir
 
@@ -18,8 +24,8 @@ mkdir -p $olddir
 echo "...done"
 
 # change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
+echo "Changing to the $DIR directory"
+cd $DIR
 echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
@@ -27,8 +33,15 @@ for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    ln -s $DIR/$file ~/.$file
 done
+
+# installing vundle
+echo "Installing vundle"
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/vundle
+
+echo "Installing vim plugins and updating it"
+vim +BundleInstall! +qall
 
 echo "Enabling new settings"
 source ~/.bash_aliases
