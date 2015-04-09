23da7108000a869c672a9e0dc8eb169e77849cfa
miter <mitermayer.reis@gmail.com>
2015-04-09 11:16:57 +1000

    Adding support for multi monitor

--- install.sh
+++ install.sh
@@ -47,6 +47,8 @@ echo "Creating symlink to ~/.i3/config file in home directory."
 
 ln -s $DIR/i3config ~/.i3/config
 
+ln -s $DIR/autoxrandr ~/bin/autoxrandr
+
 # Installing dependencies
 sudo apt-get update && sudo apt-get install curl exuberant-ctags git cmake vim-nox clang-3.5 python-dev libboost-dev python-py++ verse cowsay uuid-runtime silversearcher-ag i3 i3lock i3status pm-utils keychain ssh -y
 
